f2a4f9f807787dce3a04ac1a014f0fbb341343a8
DanielVuust <Daniel.vuust@gmail.com>
2021-02-22 09:48:28 +0100

    done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Geometri
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Square square1 = new Square();
+            Square square2 = new Square();
+            Square square3 = new Square();
+            Square square4 = new Square();
+            Square square5 = new Square();
+            square1.Side = 12;
+            square2.Side = 2;
+            square3.Side = 8;
+            square4.Side = 5;
+            square5.Side = 10;
+
+            Console.WriteLine(square1.Side);
+            Console.WriteLine(square1.omkres());
+            Console.WriteLine(square2.omkres());
+            Console.WriteLine(square3.omkres());
+            Console.WriteLine(square4.omkres());
+            Console.WriteLine(square5.omkres());
+            Console.WriteLine(square1.areal());
+            Console.WriteLine(square2.areal());
+            Console.WriteLine(square3.areal());
+            Console.WriteLine(square4.areal());
+            Console.WriteLine(square5.areal());
         }
     }
+    
 }
---
+++ Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private int side;
+        public int Side { 
+            get {
+                return this.side;
+            } 
+            set {
+                this.side = value;
+            } 
+        }
+        
+        public int omkres()
+        {
+            return 4* this.side;
+        }
+        public int areal()
+        {
+            return this.side * this.side;
+        }
+    }
+}
